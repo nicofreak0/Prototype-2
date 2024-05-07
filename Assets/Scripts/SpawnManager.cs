870a6b691ce3d45c0c1928d1087ee79547f5f76c
Lydia <146027704+nicofreak0@users.noreply.github.com>
2024-05-07 15:06:24 -0500

    random spawn animal

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    //creates a public game object array for animal prefabs
     public GameObject[] animalPrefabs;
+
+    //sets a range and position number for a spawn place
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +21,17 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // creates an if statement so that when S is pushed it generates an animal prefab in the game scene
+        if (Input.GetKeyDown(KeyCode.S)) 
+        {
+            // randomly generate animal index and spawn position
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+
+            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+            
+        }
         
     }
 }
