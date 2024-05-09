4058a2f5431c83ed00352e8a79778a1b19b5e483
Lydia <146027704+nicofreak0@users.noreply.github.com>
2024-05-09 14:06:18 -0500

    random spawn

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    public float fireDelay = 10.0f;
 
     // Update is called once per frame
     void Update()
     {
+
+        fireDelay -= 0.1F;
+
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && fireDelay <= 0.0f)
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            fireDelay = 10.0f;
         }
     }
 }
